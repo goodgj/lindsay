a0f281defe8f1453d1c3f107aed652c25c33c14a
jejuairservice <gwlee@aekyung.kr>
2024-02-18 03:14:07 +0900

    first
    first

---
+++ lindsay/wsgi.py
@@ -0,0 +1,17 @@
+"""
+WSGI config for lindsay project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os,sys 
+#sys만 새로
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lindsay.settings')
+
+application = get_wsgi_application()
---
+++ test.py
@@ -0,0 +1,5 @@
+#test.py
+import sys
+
+if __name__=='__main__':
+    print(sys.argv)
